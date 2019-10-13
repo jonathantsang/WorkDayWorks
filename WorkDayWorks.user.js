9fa866900d33bc6b48fefcb58b88841e122b1963
jonathantsang <j26tsang@gmail.com>
2019-10-12 18:23:47 -0700

    better comments

--- WorkDayWorks.user.js
+++ WorkDayWorks.user.js
@@ -4,30 +4,29 @@
 // @version      0.1
 // @description  Workday ezAce
 // @author       Jonathan T
-// @match        https://wd5.myworkday.com/*
+// @match        https://wd5.myworkday.com/linkedin/*
 // @grant        none
 // ==/UserScript==
 ​
 // If this doesn't work/messes up your timesheet I am not liable.
 ​
 window.addEventListener('load', function() {
-    // your code here
     setTimeout(function(){
+        // Get the original "Enter Time" button
         let enterTime = document.getElementsByClassName('WP3M WMQN WE5M WD4M');
         enterTime[0].click();
         setTimeout(function() {
+            // Click the "Enter Time" option in the dropdown list
             let enterTime = document.getElementsByClassName('WE4Q WA4Q WG3Q WN4Q WL4Q');
             // [0] Clear
             // [1] Enter Time
             enterTime[1].click();
             // Input the hours for 9-12, 1-6
-            // gwt-Label WMK1
             setTimeout(function(){
                 // Returns length 26 list of input boxes
                 // 4 are allocated as default for in/out
                 let inputBoxes = document.getElementsByClassName('gwt-TextBox WEE2 WLE2');
-                // TESTING for 0 index not 1-5
-                for(let i = 1; i <= 5; i++){
+                for(let i = 0; i <= 0; i++){
                     let idx = i * 4;
                     inputBoxes[idx].value = "09:00 AM";
                     inputBoxes[idx+1].value = "12:00 PM";
@@ -44,4 +43,3 @@ window.addEventListener('load', function() {
         }, 3000);
     }, 3000);
 }, false);
-
